94bdfda3a50bb47190dc038020be8356246f4cf9
Nuzhat Khan <nuzhatkhanarpa2005@gmail.com>
2026-03-31 21:25:04 +0600

    Set 1: Problem 2

---
+++ src/Set_1_beginner/Problem_2_Set_1.java
@@ -0,0 +1,14 @@
+package Set_1_beginner;
+
+import java.util.Scanner;
+
+public class Problem_2_Set_1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String username;
+        System.out.println("Enter username:");
+        username = sc.nextLine();
+        System.out.println("Hello," + username);
+    }
+}
