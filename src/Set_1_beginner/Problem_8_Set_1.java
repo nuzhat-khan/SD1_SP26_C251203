0dfc791e4a1c0331e8b45a030846f4c137b9ba11
Nuzhat Khan <nuzhatkhanarpa2005@gmail.com>
2026-04-01 12:21:17 +0600

    Updated problem 2 and 8

--- src/Set_1_beginner/Problem_8_Set_1.java
+++ src/Set_1_beginner/Problem_8_Set_1.java
@@ -9,10 +9,16 @@ public class Problem_8_Set_1 {
         int num;
         System.out.println("Enter number:");
         num = sc.nextInt();
+        System.out.print(num + "! = ");
         int i, result = 1;
         for (i = num; i >= 1; i--) {
             result = result * i;
+            if (i != 1) {
+                System.out.print(i + " X ");
+            } else {
+                System.out.print(i);
+            }
         }
-        System.out.println("The factorial of " + num + " is = " + result);
+        System.out.println(" = " + result);
     }
 }
